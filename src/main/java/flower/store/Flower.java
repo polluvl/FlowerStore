f32811574571c911b411ddc55707206d5f1e8b90
Polina Lukashenko <polinalukashenko12@gmail.com>
2025-10-08 19:44:02 +0300

    fixed all checkstyle errors

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,15 +1,54 @@
 package flower.store;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Flower {
+
     private FlowerColor color;
     private int sepalLength;
     private int price;
     private FlowerType flowerType;
+
+    public Flower(FlowerColor newColor, FlowerType newflowerType,
+    int sepalLength, int price) {
+        this.color = newColor;
+        this.flowerType = newflowerType;
+        this.sepalLength = sepalLength;
+        this.price = price;
+    }
+
+    public FlowerColor getColor() {
+        return this.color;
+    }
+
+    public void setColor(FlowerColor newcolor) {
+        this.color = newcolor;
+    }
+
+    public FlowerType getFlowerType() {
+        return this.flowerType;
+    }
+
+    public void setFlowerType(FlowerType newflowerType) {
+        this.flowerType = newflowerType;
+    }
+
+    public int getSepalLength() {
+        return this.sepalLength;
+    }
+
+    public void setSepalLength(int newsepalLength) {
+        this.sepalLength = newsepalLength;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int newprice) {
+        this.price = newprice;
+    }
 }
